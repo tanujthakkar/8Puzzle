5da3b67fc7ec15ce26b545dadf4ec4ae304863bb
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-02-18 11:04:52 -0500

    BSF works with closed_set redundancy handling

--- Code/8Puzzle.py
+++ Code/8Puzzle.py
@@ -15,6 +15,8 @@ import sys
 import os
 import numpy as np
 import argparse
+from pprint import pprint
+import time
 from multiprocessing import Queue
 
 
@@ -25,6 +27,9 @@ class Node():
         self.index = index
         self.parent_index = parent_index
         self.actions = actions
+
+    def node_summary(self):
+        pprint(self)
 
 
 class EightPuzzle():
@@ -37,21 +42,27 @@ class EightPuzzle():
                                  [-1, 0],
                                  [1, 0]]) # Action Set - RIGHT, LEFT, UP, DOWN
         self.current_index = 0
-        self.initial_node = Node(self.initial_state, self.current_index, None, self.actions)
+        self.initial_node = Node(self.initial_state, self.current_index, 0, self.actions)
         self.goal_node = Node(self.goal_state, -1, None, None)
 
         print("Created a 8 Puzzle with\n")
         print("Initial State: ", initial_state)
         print("Goal State: ", goal_state)
 
-    def find_zero(self, state):
+    def find_zero(self, state) -> np.array:
         return np.array(np.where(state==0)).flatten()
 
-    def is_valid_state(self, pos):
-        if(pos[0] > 0 and pos[0] < self.state.shape[0] and pos[1] > 0 and pos[1] < self.state.shape[1]):
+    def is_valid_state(self, pos) -> bool:
+        if(pos[0] >= 0 and pos[0] < self.initial_state.shape[0] and pos[1] >= 0 and pos[1] < self.initial_state.shape[1]):
             return True
         else:
             return False
+
+    def calc_index(self, pos) -> int:
+        return int(pos[0] * self.initial_state.shape[0] + pos[1])
+
+    def to_tuple(self, state: np.array) -> tuple:
+        return tuple(map(tuple, state))
 
     def solve(self):
 
@@ -59,34 +70,85 @@ class EightPuzzle():
 
         q.put(self.initial_node) # Pushing the current node/inital node
 
+        open_set = dict()
+        closed_set = dict()
+
+        open_set[self.initial_node.index] = self.initial_node.state
+
+        tick = time.time()
         while(q.qsize() != 0):
 
-            current_node = q.get()
-            print(current_node.state)
+            # if(self.current_index%1000):
+                # print(self.current_index)
 
-            if((current_node == goal_node).all()):
+            # if(((time.time()-tick)/60) > 10.0):
+            #     print("Time Limit Exceeded!")
+            #     return
+
+            current_node = q.get()
+            closed_set[self.to_tuple(current_node.state)] = current_node.index
+            # closed_set.add(self.to_tuple(current_node.state))
+            # print(self.to_tuple(current_node.state))
+            # input('q')
+            # open_set.pop(current_node.index)
+            # print(current_node.state)
+
+            if((current_node.state == self.goal_node.state).all()):
+                pprint(vars(current_node))
                 print("Goal Reached!")
+                toc = time.time()
+                print("Took %.03f seconds to train"%((toc-tick)))
+                q.close()
+                q.join_thread()
+                return
 
             pos = self.find_zero(current_node.state)
 
-            for action in current_node.actions:
-                new_pos = pos + action
+            for action in range(len(current_node.actions)):
+                new_pos = pos + current_node.actions[action]
                 new_state = np.copy(current_node.state)
-                if(is_valid_state(new_state)):
+                if(self.is_valid_state(new_pos)):
                     new_state[tuple(pos)], new_state[tuple(new_pos)] = new_state[tuple(new_pos)], new_state[tuple(pos)]
-                    new_index = current_index + 1
-                    new_node = Node(new_state, new_index, current_node.index, )
-                    print("State: ", new_state)
-                    input('q')
+                    new_index = self.current_index + 1
+                    # new_index = self.calc_index(new_pos)
+                    self.current_index = new_index
+                    new_action_set = np.delete(np.copy(self.actions), action, axis=0)
+                    # print(new_action_set)
+                    # print(new_action_set.reshape(len(new_action_set)//2, 2))
+                    new_node = Node(new_state, new_index, current_node.index, self.actions)
+
+                    if(self.to_tuple(new_state) in closed_set):
+                        # print("In closed_set")
+                        continue
+
+                    # if(new_index not in open_set):
+                    #     print("In open_set")
+                    #     open_set[new_index] = new_state
+                    
+                    q.put(new_node)
+
+                    if((new_node.state == self.goal_node.state).all()):
+                        pprint(vars(new_node))
+                        print("Goal Node Created!")
+                        toc = time.time()
+                        print("Took %.03f seconds to train"%((toc-tick)))
+                        # time.sleep(0.1)
+                        q.close()
+                        # q.join_thread()
+                        return True
+
+                    # pprint(vars(new_node))
+                    # input('q')
                 else:
-                    print("Invalid state: ", new_state)
+                    pass
+                    # print("Invalid state: \n", new_pos, new_state)
 
 
 def main():
 
     Parser = argparse.ArgumentParser()
-    Parser.add_argument('--InitialState', type=str, default="[[1,2,3],[4,0,5],[6,7,8]]", help='Initial state of the 8 Puzzle problem')
-    Parser.add_argument('--GoalState', type=str, default="[[1,2,3][4,5,6][7,8,0]]", help='Goal state of the 8 Puzzle problem')  
+    Parser.add_argument('--InitialState', type=str, default="[[0,1,2],[3,4,5],[6,7,8]]", help='Initial state of the 8 Puzzle problem')
+    Parser.add_argument('--GoalState', type=str, default="[[1,2,3],[4,5,6],[7,8,0]]", help='Goal state of the 8 Puzzle problem')  
 
     Args = Parser.parse_args()
     InitialState = Args.InitialState.replace('[', ' ').replace(']', ' ').replace(',', ' ').split()
